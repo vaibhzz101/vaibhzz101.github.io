3bd27f0a007493d8351c3431d89aed1296fad8b5
vaibhzz101 <vaibhzzsmiley@gmail.com>
2023-03-17 02:48:08 +0530

    ad

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 
 
@@ -50,64 +50,6 @@ window.addEventListener("scroll", () => {
 
 
 
-/**
- * SLIDER
- */
 
-const slider = document.querySelector("[data-slider]");
-const sliderContainer = document.querySelector("[data-slider-container]");
-const sliderPrevBtn = document.querySelector("[data-slider-prev]");
-const sliderNextBtn = document.querySelector("[data-slider-next]");
 
-let totalSliderVisibleItems = Number(getComputedStyle(slider).getPropertyValue("--slider-items"));
-let totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
 
-let currentSlidePos = 0;
-
-const moveSliderItem = function () {
-  sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
-}
-
-/**
- * NEXT SLIDE
- */
-
-const slideNext = function () {
-  const slideEnd = currentSlidePos >= totalSlidableItems;
-
-  if (slideEnd) {
-    currentSlidePos = 0;
-  } else {
-    currentSlidePos++;
-  }
-
-  moveSliderItem();
-}
-
-sliderNextBtn.addEventListener("click", slideNext);
-
-/**
- * PREVIOUS SLIDE
- */
-
-const slidePrev = function () {
-  if (currentSlidePos <= 0) {
-    currentSlidePos = totalSlidableItems;
-  } else {
-    currentSlidePos--;
-  }
-
-  moveSliderItem();
-}
-
-sliderPrevBtn.addEventListener("click", slidePrev);
-
-/**
- * RESPONSIVE
- */
-window.addEventListener("resize", function () {
-  totalSliderVisibleItems = Number(getComputedStyle(slider).getPropertyValue("--slider-items"));
-  totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
-
-  moveSliderItem();
-});
