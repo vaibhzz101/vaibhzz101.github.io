6c2014f35ed875bedb2f165fb2f56fb88d9bd1be
vaibhzz101 <vaibhzzsmiley@gmail.com>
2023-03-21 18:00:36 +0530

    made respomsive

--- script.js
+++ script.js
@@ -18,22 +18,47 @@ const addEventOnElements = function (elements, eventType, callback) {
  * MOBILE NAVBAR TOGGLER
  */
 
-const navbar = document.querySelector("[data-navbar]");
-const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+// const navbar = document.querySelector("[data-navbar]");
+// const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 
-const toggleNav = () => {
-  navbar.classList.toggle("active");
-  document.body.classList.toggle("nav-active");
+// const toggleNav = () => {
+//   navbar.classList.toggle("active");
+//   document.body.classList.toggle("nav-active");
+// }
+
+// addEventOnElements(navTogglers, "click", toggleNav);
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("#nav-ul");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 }
 
-addEventOnElements(navTogglers, "click", toggleNav);
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+let resume1 = document.getElementById("resume-button-1").addEventListener("click", NewTab)
+let resume2 = document.getElementById("resume-button-2").addEventListener("click", NewTab)
 
 
 
-/**
- * HEADER ANIMATION
- * When scrolled donw to 100px header will be active
- */
+function NewTab() {
+  window.open(
+    "https://drive.google.com/file/d/1PWDFAedYbOU7LGcKE9O8SNiY1aUOCr_N/view?usp=share_link",
+    "_blank"
+  );
+}
+
+
 
 const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
