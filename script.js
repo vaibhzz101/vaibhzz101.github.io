491c56084b5fe5fa82b993faa5c586027a831ded
Vaibhav Shevne <112811407+vaibhzz101@users.noreply.github.com>
2023-03-23 17:05:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -28,24 +28,6 @@ const addEventOnElements = function (elements, eventType, callback) {
 
 // addEventOnElements(navTogglers, "click", toggleNav);
 
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector("#nav-ul");
-
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-}
-
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach((n) => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}
 let resume1 = document.getElementById("resume-button-1").addEventListener("click", NewTab)
 let resume2 = document.getElementById("resume-button-2").addEventListener("click", NewTab)
 
