597832a9473204baee1f782239b5ccd820b88738
vaibhzz101 <vaibhzzsmiley@gmail.com>
2023-03-23 15:05:02 +0530

    changes

--- script.js
+++ script.js
@@ -60,18 +60,18 @@ function NewTab() {
 
 
 
-const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
+// const header = document.querySelector("[data-header]");
+// const backTopBtn = document.querySelector("[data-back-top-btn]");
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
-});
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 100) {
+//     header.classList.add("active");
+//     backTopBtn.classList.add("active");
+//   } else {
+//     header.classList.remove("active");
+//     backTopBtn.classList.remove("active");
+//   }
+// });
 
 
 
