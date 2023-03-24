351ecad917bdf1ef3d06eb17cfb4d0865c841355
vaibhzz101 <vaibhzzsmiley@gmail.com>
2023-03-25 03:35:26 +0530

    animation added late night work

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
+// *=============================== toggle icon navbar ==================================
 
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('#nav-menu')
 
-
-
-/**
- * Add event listener on multiple elements
- */
-
-const addEventOnElements = function (elements, eventType, callback) {
-  for (let i = 0, len = elements.length; i < len; i++) {
-    elements[i].addEventListener(eventType, callback);
-  }
-}
-
-
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active')
+};
 
     GitHubCalendar(".calendar", "vaibhzz101");
 
@@ -38,7 +32,7 @@ const addEventOnElements = function (elements, eventType, callback) {
       );
     }
 
-
+// *=============================== scroll sections active link ==================================
 let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a')
 
@@ -53,15 +47,43 @@ window.onscroll = () => {
         navlinks.forEach(links => {
           links.classList.remove('active');
           document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
-        })
+        });
     };
 
-  })
-}
+  });
+  // *=============================== sticky navbar ==================================
+   
+  let header = document.querySelector('header');
 
+  header.classList.toggle('sticky', window.scrollY > 100);
 
+// *=============================== remove toggle icon and navbar when click navbar link(scroll) ==================================
 
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active')
 
+};
 
+// *=============================== scroll reveal ==================================
 
+ScrollReveal({
+  // reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
 
+ScrollReveal().reveal('.home-content, .heading', { origin:'top'});
+ScrollReveal().reveal('.home-img, .skills-container, .project-card, .contact form', { origin:'bottom'});
+ScrollReveal().reveal('.home-content h1', { origin:'left'});
+ScrollReveal().reveal('.home-content p, .about-content', { origin:'left'});
+
+// *===============================  typed js==================================
+
+const typed = new Typed('.multiple-text', {
+  strings: ['Full Stack Web Developer', 'Backend Developer', 'Frontend Developer'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backdelay: 1000,
+  loop: true
+})
